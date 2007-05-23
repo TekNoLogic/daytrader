7fc507af7cf201b244f116b786940815136199a1
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-05-23 02:58:20 +0000

    Importing DayTrader, an alt-click-to-trade addon.
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/DayTrader@400 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ DayTrader.lua
@@ -0,0 +1,48 @@
+--[[ Alt-Click to add an item to a mail for sending or to the trade frame. ]]--
+
+-- Most of this code is grennon's from AltClickToAddItem, I just removed the dependancy on AceHooks
+-- AltClickToAddItem based on GMail 1.x which was ported from CT_MailMod pre-1.5
+
+local targ, bag, slot
+
+
+local orig1 = ContainerFrameItemButton_OnModifiedClick
+ContainerFrameItemButton_OnModifiedClick = function(button, ...)
+  if button == "LeftButton" and IsAltKeyDown() and not CursorHasItem() then
+    bag, slot = this:GetParent():GetID(), this:GetID()
+		if TradeFrame:IsVisible() then
+      for i=1,6 do
+        if not GetTradePlayerItemLink(i) then
+					PickupContainerItem(bag, slot)
+          ClickTradeButton(i)
+					bag, slot = nil, nil
+					return
+        end
+      end
+    elseif not CursorHasItem() and UnitExists("target") and UnitIsFriend("player", "target") and UnitIsPlayer("target") and CheckInteractDistance("target", 2) then
+      targ = UnitName("target")
+      InitiateTrade("target")
+      return
+    end
+  end
+  orig1(button, ...)
+end
+
+
+local function posthook(...)
+  if targ and not CursorHasItem() and UnitName("target") == targ then
+    PickupContainerItem(bag, slot)
+    ClickTradeButton(1)
+  end
+  targ, bag, slot = nil, nil, nil
+
+	return ...
+end
+
+
+local orig2 = TradeFrame:GetScript("OnShow")
+TradeFrame:SetScript("OnShow", function(...)
+	if orig2 then return posthook(orig2(...))
+	else posthook() end
+end)
+
